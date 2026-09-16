4dc15495872d7c85483b1f828e278d9a92acb598
bhumeesethi <bhumeesethi31@gmail.com>
2026-09-16 19:42:21 +0530

    Add allowedDevOrigins and iOS apple-touch-icons

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ['192.168.29.98', 'localhost:3000'],
+  reactStrictMode: false,
+};
+
+module.exports = nextConfig;
